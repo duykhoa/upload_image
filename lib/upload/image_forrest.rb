51f5f077c025f15da19039b943779c8c2cdc5435
Duy Khoa <duykhoa12t@gmail.com>
2015-11-16 18:02:50 +0800

    Add tomdoc for image_forrest & success response class

--- lib/upload/image_forrest.rb
+++ lib/upload/image_forrest.rb
@@ -6,9 +6,24 @@ module Upload
 
     attr_reader :image
 
+    # Public: Init a ImageForrest Object
+    #
+    # path  - Image's path, right now must be an absolute path
+    #
+    # Example
+    #
+    #     path = '/Users/kevin/Desktop/image.jpeg'
+    #     image_forrest = ImageForrest.new(path)
+    #
+    # Returns an ImageForrest object
+    #
+    # TODO:
+    #   -  path can be a relative path
+    #   -  how about upload a list of images (idea)
     def initialize(path)
       @image = Image.new(path)
     end
+
 
     def upload
       return Response::Failure.new("not_exist") unless @image.exist?
--- lib/upload/response/success.rb
+++ lib/upload/response/success.rb
@@ -4,6 +4,13 @@ module Upload
       def initialize(response, opts={})
         super(response, opts)
 
+        # Use thumb url instead of the original one
+        # The thumb url has fixed width for now (500px)
+        # Will be configuarable soon.
+        # TODO:
+        #   - Check the data field before return
+        #   - Allow user to config the size of thumb
+        #
         @url = response["data"]["thumb_url"]
 
         # carefully check the data key here
